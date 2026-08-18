8a90e75891cc021c805def487010749dcb2c6599
Vinodini Ilam <vinodiniilam@gmail.com>
2026-08-18 15:16:20 +0530

    Frequency count of char in string using function

---
+++ DAY1/frequency.py
@@ -0,0 +1,10 @@
+def count_char():
+    string = input("enter a string: ")
+    count=0
+    for ch in string:
+        if ch=='i':
+            count=count+1
+    print(count)
+count_char()    
+      
+            
