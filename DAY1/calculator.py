33923d7f9ae98cd5f217eb8192a053c747d89507
Vinodini Ilam <vinodiniilam@gmail.com>
2026-08-18 15:32:38 +0530

    Calculator using functions

---
+++ DAY1/calculator.py
@@ -0,0 +1,12 @@
+def mini_calculator(num1,num2,operator):
+    if operator=='+':
+        return num1+num2
+    elif operator=='-':
+        return num1-num2
+    elif operator=='*':
+        return num1*num2
+    elif operator=='/':
+        return num1/num2
+    else:    
+        return "invailid operator"
+print(mini_calculator(10,20,'//'))
