8997e5b1ab9b68d2a791bcdd1233ab9dd13602a7
Vinodini Ilam <vinodiniilam@gmail.com>
2026-08-22 22:18:39 +0530

    calci

--- L1-VARIABLES/operators.py/calculator.py
+++ L1-VARIABLES/operators.py/calculator.py
@@ -1,4 +1,4 @@
-a=int(input("Enter the first number: "))
+"""a=int(input("Enter the first number: "))
 b=int(input("Enter the second number: "))
 op=input("Enter the operator (+,-,*,/): ")
 if op=="+":
@@ -11,3 +11,9 @@ elif op=="/":
     print(a/b)
 else:
     print("Invalid operator")
+#/ gives the floating point // gives the integer value
+# / gives the quotient 
+# % gives remainder
+"""
+n=5
+print(n^3)
