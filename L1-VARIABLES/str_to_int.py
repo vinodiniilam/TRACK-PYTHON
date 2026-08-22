b221f3b69488b4bd35673b56807d44d52ab759dc
Vinodini Ilam <vinodiniilam@gmail.com>
2026-08-22 19:36:19 +0530

    Different types of conversions

---
+++ L1-VARIABLES/str_to_int.py
@@ -0,0 +1,30 @@
+a="100"# it gives integer because it is a number it is a string we can convert to number
+a=int(a)
+print(a)
+print(type(a))
+
+#str  to float
+
+b="200" # if we gave name here it gives error because it is not a number it is a string we can not convert to number
+b=float(b)
+print(b)
+print(type(b))
+
+#int to float
+x=10
+print(type(x))
+x=float(x)
+print(x)
+print(type(x))
+#integer to string 
+a=10
+print(type(a))
+a=str(a)
+print(a)
+print(type(a))
+print("Value is" +a)
+
+
+
+
+
