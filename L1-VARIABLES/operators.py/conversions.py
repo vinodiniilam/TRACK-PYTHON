432ff3a21555e7b236bc9bb43bf65656100601ce
Vinodini Ilam <vinodiniilam@gmail.com>
2026-08-22 22:54:52 +0530

    conversion of time,distance,temp,profit and loss ,age calculations

---
+++ L1-VARIABLES/operators.py/conversions.py
@@ -0,0 +1,39 @@
+"""#formula to convert the celsius into fahrenheit
+#f=(c*9/5)+32
+temp=43
+fahrenheit=(temp*9/5)+32
+print(fahrenheit)
+#formula to convert fahrenheit to celcius
+#c=(f-32)*5/9
+fp=109.4
+celcius=(fp-32)*5/9
+print(celcius)
+#distance conversion
+#1km=1000meters
+#1m=100cm
+#1mile=1.609km
+d=10
+meters=d*1000
+print(meters)
+#time conversion
+#1H=60 min 1m=60 sec
+n=3665
+M=n//60
+H=M//60
+sec_5=M%60
+print(sec_5,M,H)
+#age caluculation
+birth_year=2005
+current_year=2026
+age=current_year-birth_year
+print(age)"""
+#profit loss
+sp=1500
+cp=1200
+profit=sp-cp
+loss=cp-sp
+print(profit)
+print(loss)
+
+
+
