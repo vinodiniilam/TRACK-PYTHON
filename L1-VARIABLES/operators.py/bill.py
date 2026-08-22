ade9bc562cb83fc2ae14f5f4c5a72b6debdbe6fc
Vinodini Ilam <vinodiniilam@gmail.com>
2026-08-22 22:34:10 +0530

    bill calculation including gst,tax,discount

---
+++ L1-VARIABLES/operators.py/bill.py
@@ -0,0 +1,15 @@
+"""price=2000
+discount_amount=(10/100)*price
+gst_amount=(18/100)*(price-discount_amount)# for gst we have to consider the total amount as after removing the discount amount that is 1800
+final_amount=(price-discount_amount)+gst_amount
+print(discount_amount)#10% discount on 2000 is 200
+print(gst_amount)#18% gst on 1800 is 324
+print(final_amount)# final amount including gst is 2124"""
+
+#calculating total salary
+basic_salary=20000
+house_rent=6000
+others=7000
+total_salary=basic_salary+house_rent+others
+print(total_salary)
+
