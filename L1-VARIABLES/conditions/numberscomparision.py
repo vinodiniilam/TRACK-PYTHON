f3ca3ff6a4a450e8989a44ea1c38bbf36ac573ae
Vinodini Ilam <vinodiniilam@gmail.com>
2026-08-26 19:23:13 +0530

    comparing the 2 and 3 numbers and returning the largest and smallest among them.

---
+++ L1-VARIABLES/conditions/numberscomparision.py
@@ -0,0 +1,45 @@
+#comparing two numbers and returning the small and large numbers.
+"""a=int(input("enterthe first number: "))
+b=int(input("enter the second number: "))
+if a>b:
+    small=b
+    large=a
+else:
+    small=a
+    large=b
+print("the smaller number is:",small)
+print("the larger number is:",large)
+#comapring the three numbers and returning the largest number.
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+c=int(input("enter the third number: "))
+if a>b and a>c:
+    large=a
+elif b>a and b>c:
+    large=b
+else:
+    large=c
+print("the larger number is:",large)   
+#comparing three numbers and returning the smallest number.and also checking the if they are equal or not
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+c=int(input("enter the third number: "))
+if a<b and a<c:
+    small=a
+elif b<a and b<c:
+    small=b
+else:
+    small=c
+if a==b and b==c:
+    print("the numbers are equal")    
+else:
+    print("the smallest number is:",small)    """
+n=int(input("enter the first number: "))    
+m=int(input("enter the second number: "))
+if n>m:
+    print("the first number is greater than the second number")
+elif n<m:
+    print("the second number is greater than the first number")
+else:
+    print("the numbers are equal")    
+    
