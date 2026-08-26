3d97ca49cc74b27b8d9b7ff6606b824892732788
Vinodini Ilam <vinodiniilam@gmail.com>
2026-08-26 22:24:35 +0530

    finding the leap year and finding month days and names

---
+++ L1-VARIABLES/yearproblems.py
@@ -0,0 +1,15 @@
+year=int(input("enter the year: "))
+if year%400!=0 and year%4!=0 and year%100!=0:
+    print("the year is not a leap year")
+else:
+    print("the year is a leap year")    
+#finding month days by using month number
+n=int(input("enter the month number: "))
+if n==1 or n==3 or n==5 or n==7 or n==8 or n==10 or n==12:
+    print("the month has 31 days")
+elif n==4 or n==6 or n==9 or n==11:
+    print("the month has 30 days")
+elif n==2:
+    print("the month has 28 or 29 days")
+else:
+    print("invalid month number") 
