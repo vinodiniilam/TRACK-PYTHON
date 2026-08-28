9fe45112232818194c2eaf937b4dee57f4d45dd3
Vinodini Ilam <vinodiniilam@gmail.com>
2026-08-28 15:20:56 +0530

    character and number classifcation.

--- L1-VARIABLES/nested_real_world.py
+++ L1-VARIABLES/nested_real_world.py
@@ -13,6 +13,45 @@ if withdrwal<=amount:
         print("invalid amount")
 else:
     print("insufficient balance")
+#ATM WITHDRAWL
+pin=int(input("enter the pin: "))
+amount=int(input("enter the amount"))
+balance_amount=int(input("enter the balance_amount"))
+if pin=="1234":
+    if balance_amount%100==0:
+        if amount<=balance_amount:
+            print("Withdrawl Successfull")
+        else:
+            print("Insufficient Balance")
+    else:
+        print("Please enter the amount in multiple of 100")
+else:
+    print("Invalid Pin")
+#collage admission
+percentage=int(input("enter the percentage: "))
+score=int(input("enter the score: "))
+if score>=60:
+    if percentage>=75:
+        print("Admission Eligible")
+    else:
+        print("required percentage not obtained")
+else:
+    print("score is insufficient")
+
+degree=input("enter the qualification degree: ")
+percentage=int(input("enter the percentage: "))
+YOP=int(input("enter the year of passing: "))
+if degree=="B.Tech":
+    if percentage>=75:
+        if YOP>=2025:
+            print("Shortlisted")
+        else:
+            print("Not short listed")
+    else:
+        print("percentage does not meet") 
+else:
+    print("Qualification is not eligible")   
+
 
 
     
